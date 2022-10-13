23c531be1cba2732890f73d11a2d372bf9ff38ea
sanjaymehar <104576377+sanjaymehar@users.noreply.github.com>
2022-10-13 11:25:46 +0530

    Update urls.py

--- proj2/urls.py
+++ proj2/urls.py
@@ -12,9 +12,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 #    path('', include(router.urls)),
     path('',YourModelView.as_view()),
+    path('createuser/',CreateUserView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
