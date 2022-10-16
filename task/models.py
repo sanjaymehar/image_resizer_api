62c3a02ca4d1699b1c93f71b79b1c6754c21cd36
Sanjay Mehar <104576377+sanjaymehar@users.noreply.github.com>
2022-10-16 20:42:31 +0530

    Update models.py

--- task/models.py
+++ task/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from .manager import *
-
+from django.dispatch import receiver
+import os
 
 class User(AbstractUser):
     username = None
@@ -22,3 +23,29 @@ class Eimage(models.Model):
     thumbnail=models.ImageField(upload_to='images')
     medium=models.ImageField(upload_to='images')
     large=models.ImageField(upload_to='images')
+    
+    
+@receiver(models.signals.post_delete, sender=Eimage)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.gray:
+        if os.path.isfile(instance.gray.path):
+            os.remove(instance.gray.path)
+
+    if instance.thumbnail:
+        if os.path.isfile(instance.thumbnail.path):
+            os.remove(instance.thumbnail.path)
+
+    if instance.medium:
+        if os.path.isfile(instance.medium.path):
+            os.remove(instance.medium.path)
+
+    if instance.large:
+        if os.path.isfile(instance.large.path):
+            os.remove(instance.large.path)
+
+@receiver(models.signals.post_delete, sender=Imagee)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+        
